6c80de4f66075eb929872163de9d607f5b3a2c70
María Rosa Aravid Martín <maravid@uoc.edu>
2023-11-21 02:09:00 +0100

    Primer commit de PEC1

---
+++ dist/index.3d497628.js
@@ -0,0 +1,3 @@
+console.log("Hello world");//# sourceMappingURL=index.3d497628.js.map
+
+//# sourceMappingURL=index.3d497628.js.map
---
+++ dist/index.bff8c4ed.js
@@ -0,0 +1,3 @@
+console.log("Hello world");//# sourceMappingURL=index.bff8c4ed.js.map
+
+//# sourceMappingURL=index.bff8c4ed.js.map
